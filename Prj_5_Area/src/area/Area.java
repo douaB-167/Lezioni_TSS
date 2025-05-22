969eee97311acab6c99e8e30d010b4f8378c6de7
douaB-167 <doua.boufqir@edu.immaginazioneelavoro.it>
2025-05-22 14:22:40 +0200

    Add files via upload

---
+++ Prj_5_Area/src/area/Area.java
@@ -0,0 +1,24 @@
+package area;
+
+public class Area {
+	
+	protected double base;
+	protected double altezza;
+	protected double raggio;
+	
+	public Area(double base, double altezza) {
+		this.base = base;
+		this.altezza = altezza;
+	}
+	
+	public Area(double raggio) {
+		this.raggio = raggio;
+	}
+	
+	public double calcoloArea() {
+		double area = 0; 
+		
+		return 0;
+	}
+
+}
