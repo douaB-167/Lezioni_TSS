a20eccc472df06fcaa5d9e42c354396e84cbd6dc
douaB-167 <doua.boufqir@edu.immaginazioneelavoro.it>
2025-05-23 09:20:58 +0200

    Update FormaGeometrica.java

--- Prj_5_Area/src/area/FormaGeometrica.java
+++ Prj_5_Area/src/area/FormaGeometrica.java
@@ -5,8 +5,6 @@ public class FormaGeometrica {
 	protected double lato;
 	protected double base;
 	protected double altezza;
-	protected double a;
-	protected double b;
 	
 	public FormaGeometrica(double lato) {
 		this.lato = lato;
@@ -15,12 +13,6 @@ public class FormaGeometrica {
 	public FormaGeometrica(double base, double altezza) {
 		this.base = base;
 		this.altezza = altezza;
-	}
-	
-	public FormaGeometrica(double base, double a, double b) {
-		this.base = base;
-		this.a = a;
-		this.b = b;
 	}
 	
 	public double calcolaArea() {
